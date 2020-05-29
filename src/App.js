b27682fcbe31b2abbbba134992eb8a69d1133aee
João Vitório <61637710+jaovito@users.noreply.github.com>
2020-05-29 11:51:46 -0300

    Calculator

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Boa noite amor.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Te Amo!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
